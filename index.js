91ec15d1d72c76f3d7c2d0d549e0391775c6e719
sodicksafety <sodicksafety@gmail.com>
2026-03-29 17:10:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const line = require("@line/bot-sdk");
+const axios = require("axios");
+
+const app = express();
+
 /* --------------------------------------------------
    CONFIG
 -------------------------------------------------- */
